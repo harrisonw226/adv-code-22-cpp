55fae6e31dd072890f2d7189bd02300a986fdbc8
harrisonw <harrisonw@bytesnap.co.uk>
2023-09-19 16:17:51 +0100

    day3 part 2

--- day3.cpp
+++ day3.cpp
@@ -7,36 +7,52 @@
 
 using namespace std;
 
+int get_badge_val(char c){
+	int badge_val;
+	cout << c << endl;
+	cout << (int) c << endl;
+	if (isupper(c)) {
+		badge_val = (int) c - 38;
+	}
+	else {
+		badge_val = (int) c - 96; 
+	}
+	cout << badge_val << endl;
+	return badge_val;
+}
+
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) return -1;
 		
 	ifstream file(argv[1]);
 	string line;
-	int badge_val = 0;
 	int badge_val_tot = 0;
+	int mod = 0;
+	string bag1;
+	string bag2;
+	string bag3;
 	while(std::getline(file, line)) {
 		cout << line << endl;
-		string bag1 = string(line, 0, (line.length())/2);
-		string bag2 = string(line, (line.length())/2, line.length());
-		
-		for (auto c : bag1) {
-			if ( bag2.find_first_of(c) != std::string::npos){ 
-				cout << c << endl;
-				cout << (int) c << endl;
-				if (isupper(c)) {
-					badge_val = (int) c - 38;
-				}
-				else {
-					badge_val = (int) c - 96; 
-				}
-				cout << badge_val << endl;
-				badge_val_tot += badge_val;
-				break;
+		switch (mod % 3){
+		case (0): 
+			bag1 = std::move(line); 
+			break;
+		case (1): 
+			bag2 = std::move(line);
+			break;
+		case (2):
+			bag3 = std::move(line); 
+			for (auto c : bag1){
+					if ( bag2.find_first_of(c) != string::npos && bag3.find_first_of(c) != string::npos) {
+						cout << c << endl;
+						badge_val_tot += get_badge_val(c);
+						break;
+					}
 			}
 		}
-
+		mod += 1;
 	}
-	cout << "total: " << badge_val_tot << endl;
+	cout << "badge tot: " << badge_val_tot << endl;
 	file.close();
 }
